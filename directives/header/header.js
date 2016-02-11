cdf53153ae2efbbb77d54415eba36bbdb47c6724
Blaise Fonseca <blaisefonseca@hotmail.com>
2016-02-11 16:56:52 -0500

    remove usernotification ref from header
    Signed-off-by: Blaise Fonseca <blaisefonseca@hotmail.com>

--- directives/header/header.js
+++ directives/header/header.js
@@ -1,33 +1,30 @@
-
 define(['app',
- 'text!./header.html',
-  'jquery',
-  'css!/app/libs/scbd-branding/css/colors',
-  'css!./header',
-  'scbd-angularjs-services/authentication',
-  './account',
-  './locale',
-  './accounts-validation',
-  './login',
-  './xuser-notifications',
-  './xuser-notifications-icon',
-  'scbd-angularjs-services/user-notifications'
-],
-function(app, template, $) {
-     app.directive('scbdHeader', function() {
-         return { restrict: 'E' ,
-                  priority: 10, //parent has 0 priority
-                  template: template,
+        'text!./header.html',
+        'jquery',
+        'css!/app/libs/scbd-angularjs-branding/css/colors',
+        'css!./header',
+        'scbd-angularjs-services/authentication',
+        './account',
+        './locale',
+        './accounts-validation',
+        './login',
+        './xuser-notifications-icon'
+    ],
+    function(app, template, $) {
+        app.directive('scbdHeader', function() {
+            return {
+                restrict: 'E',
+                priority: 10, //parent has 0 priority
+                template: template,
+                controller: ['$scope', 'authentication',
+                    function($scope, authentication) {
+                        authentication.getUser().then(function(u) {
+                            $scope.user = u;
+                            $scope.toggleMenu = 0;
 
-                  controller: ['$scope','$element', '$attrs','$window', '$location','authentication','IUserNotifications',
-                  function($scope, $element, $attrs,$window, $location,authentication,userNotifications) {
-                      authentication.getUser().then(function(u){
-                    	     $scope.user = u;
-                           $scope.toggleMenu=0;
-                           $scope.userNotifications=userNotifications;
-
-                			});
-                  }],//controller
-        };//return
-     });//directive
-});
+                        });
+                    }
+                ], //controller
+            }; //return
+        }); //directive
+    });
